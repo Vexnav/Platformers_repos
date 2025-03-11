b40b38cfc369275c3bfffca3960872886c627a67
MthokozisiNyawo <mthokozisiemmanuel12@gmail.com>
2025-03-11 17:42:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "Plaformers")
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "sqlite:///lost.db"
+        "sqlite:///lost_and_found.db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
