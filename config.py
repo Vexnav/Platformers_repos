b507128df67e80f63de3b924968a244fccd4383c
Dilan <dilannaid123@gmail.com>
2025-03-25 13:57:35 +0200

    Final Commit

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 from flask_mail import Mail
 
 class Config:
-    SECRET_KEY = 'Platformers'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Nkambule%40123@localhost/lost_and_found'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'Platformers') 
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://root:JXDL86@localhost/platformersdatabase')
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static/images')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'dutlostandfound@gmail.com'  
-    MAIL_PASSWORD = 'jyhw jjpm gcjr tvmz'        
-    MAIL_DEFAULT_SENDER = 'dutlostandfound.ac.za'
+    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
+    MAIL_USE_TLS = bool(os.getenv('MAIL_USE_TLS', True))
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'dutlostandfoundportal@gmail.com')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'shas xavx iptu bpmy')  
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'lostandfoundportal@dut4life.ac.za')
 
 mail = Mail()
